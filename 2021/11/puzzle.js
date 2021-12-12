d65506137c81c0e61b8168f929e7c4bb887de486
Cameron Dubas <camerondubas@gmail.com>
2021-12-12 00:58:14 -0800

    Clean up day 11

--- 2021/11/puzzle.js
+++ 2021/11/puzzle.js
@@ -1,49 +1,24 @@
-const size = 10;
-
-const flash = (index, grid) => {
-  if (grid[index] === 0) {
-    return;
-  }
-
-  grid[index] = 0;
-
-  const topOffsets = [-size - 1, -size, -size + 1];
-  const bottomOffsets = [size - 1, size, size + 1];
-  const leftOffsets = [-1, size - 1, -size - 1];
-  const rightOffsets = [size + 1, 1, -size + 1];
+const flash = (_x, _y, grid) => {
+  grid[_y][_x] = 0;
 
   let offsets = [
-    ...new Set([
-      0,
-      ...bottomOffsets,
-      ...topOffsets,
-      ...leftOffsets,
-      ...rightOffsets,
-    ]),
+    [0, 0], [0, 1], [0, -1],
+    [1, 0], [1, 1], [1, -1],
+    [-1, 0], [-1, 1], [-1, -1],
   ];
-
-  const isTop = Math.floor(index / size) === 0;
-  const isBottom = Math.floor(index / size) === size - 1;
-  const isLeft = index % size === 0;
-  const isRight = index % size === size - 1;
-
-  offsets = offsets
-    .filter((i) => (isLeft ? !leftOffsets.includes(i) : true))
-    .filter((i) => (isRight ? !rightOffsets.includes(i) : true))
-    .filter((i) => (isTop ? !topOffsets.includes(i) : true))
-    .filter((i) => (isBottom ? !bottomOffsets.includes(i) : true));
-
+  
   offsets.forEach((offset) => {
-    let idx = index + offset;
+    let x = _x + offset[0];
+    let y = _y + offset[1];
 
-    if (grid[idx] === 0) {
+    if (grid[y]?.[x] === undefined || grid[y]?.[x] === 0) {
       return;
     }
 
-    grid[idx] = grid[idx] + 1;
+    grid[y][x] = grid[y][x] + 1;
 
-    if (grid[idx] > 9) {
-      flash(idx, grid);
+    if (grid[y][x] > 9) {
+      flash(x, y, grid);
     }
   });
 };
@@ -51,22 +26,24 @@ const flash = (index, grid) => {
 const output = (input) => {
   let octopuses = input
     .split("\n")
-    .map((row) => row.split("").map((num) => parseInt(num, 10)))
-    .flat();
+    .map((row) => row.split("").map((num) => parseInt(num, 10)));
 
   const steps = 100;
   let flashesCount = 0;
 
   for (let stepIndex = 0; stepIndex < steps; stepIndex++) {
-    octopuses = octopuses.map((octopus) => octopus + 1);
-    octopuses.forEach(
-      (octopus, index) => octopus > 9 && flash(index, octopuses)
-    );
+    octopuses = octopuses.map((row) => row.map((octopus) => octopus + 1));
+    for (let y = 0; y < octopuses.length; y++) {
+      for (let x = 0; x < octopuses[y].length; x++) {
+        if (octopuses[y][x] > 9) {
+          flash(x, y, octopuses);
+        }
+      }
+    }
 
-    flashesCount = octopuses.reduce(
-      (prev, cur) => (cur === 0 ? prev + 1 : prev),
-      flashesCount
-    );
+    flashesCount = octopuses
+      .flat()
+      .reduce((prev, cur) => (cur === 0 ? prev + 1 : prev), flashesCount);
   }
   return flashesCount;
 };
