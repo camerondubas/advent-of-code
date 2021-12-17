e2e34b4e0f4fc66e07b762d23c7b5893b18cf13d
Cameron Dubas <camerondubas@gmail.com>
2021-12-17 13:08:49 -0800

    Add total runtime to metrics

--- cli/run.js
+++ cli/run.js
@@ -34,12 +34,15 @@ const getRunner = (data) => {
       console.log("------------------");
       let runtimes = [];
       let output;
+      let t0b = performance.now();
       for (let index = 0; index < data.metricsRuns; index++) {
         let t0 = performance.now();
         output = puzzle(input);
         let t1 = performance.now();
         runtimes.push(t1 - t0);
       }
+      let t1b = performance.now();
+
       const runtimesSum = runtimes.reduce((acc, cur) => acc + cur, 0);
       console.log("Number of Runs: ", data.metricsRuns);
       console.log(
@@ -48,6 +51,7 @@ const getRunner = (data) => {
       );
       console.log("Slowest Run (ms):", Math.max(...runtimes).toFixed(4));
       console.log("Fastest Run (ms):", Math.min(...runtimes).toFixed(4));
+      console.log("Total Runtime (ms):", (t1b - t0b).toFixed(4));
       console.log("Output: ", output);
       console.log("==================");
     };
