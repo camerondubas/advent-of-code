48603eb2abf159f8b7abaa829b56ee4897842d6f
Cameron Dubas <camerondubas@gmail.com>
2021-12-19 12:07:45 -0800

    Fetch Readme in generate method

--- cli/generate.js
+++ cli/generate.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const fse = require("fs-extra");
 const fetch = require("node-fetch");
+const TurndownService = require("turndown");
+const turndownService = new TurndownService();
 
 const generate = async (year, day) => {
   let srcDir = path.join(global.appRoot, "__template__");
@@ -13,7 +15,22 @@ const generate = async (year, day) => {
     },
   });
   data = await data.text();
+
   fse.writeFileSync(path.join(destDir, "input.txt"), data, {
+    overwrite: false,
+  });
+
+  let readme = await fetch(`https://adventofcode.com/${year}/day/${day}`, {
+    headers: {
+      cookie: `session=${process.env.SESSION_ID}`,
+    },
+  });
+  readme = await readme.text();
+
+  let regex = new RegExp(/(\<article[\w|\s|=|"|-]*>)(.*)(<\/article>)/, "gs");
+  let markdown = turndownService.turndown(regex.exec(readme)[2]);
+
+  fse.writeFileSync(path.join(destDir, "README.md"), markdown, {
     overwrite: false,
   });
 };
