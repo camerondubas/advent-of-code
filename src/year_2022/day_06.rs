7177251fc591f3a917b68f364af1bae1e5e56542
Cameron Dubas <camerondubas@gmail.com>
2022-12-08 01:05:50 +0000

    2022 06 rust cleanup

--- src/year_2022/day_06.rs
+++ src/year_2022/day_06.rs
@@ -7,31 +7,32 @@ pub fn solution(contents: String) {
     run("Part 2 Bytes", part_2_bytes, &contents);
 }
 
-fn get_window_index(input: &String, size: usize) -> usize {
+fn get_position(input: &String, size: usize) -> usize {
     input
         .as_bytes()
         .windows(size)
         .find_position(|window| window.iter().all_unique())
         .unwrap()
         .0
+        + size
 }
 fn part_1(input: &String) -> usize {
     let packet_size = 4;
-    get_window_index(input, packet_size) + packet_size
+    get_position(input, packet_size)
 }
 
 fn part_2(input: &String) -> usize {
-    let packet_size = 14;
-    get_window_index(input, packet_size) + packet_size
+    let message_size = 14;
+    get_position(input, message_size)
 }
 
 fn part_2_bytes(input: &String) -> usize {
-    let packet_size = 14;
+    let message_size = 14;
     input
         .as_bytes()
-        .windows(packet_size)
+        .windows(message_size)
         .find_position(|window| window.iter().all_unique())
         .unwrap()
         .0
-        + packet_size
+        + message_size
 }
