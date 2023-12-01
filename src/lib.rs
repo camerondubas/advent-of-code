4c2e7101413ca26c6bde13292fcdcfcc5d539fe5
Cameron Dubas <camerondubas@gmail.com>
2023-12-01 22:47:26 +0000

    test: 2023 01

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
+use std::fs;
+
 pub mod runner;
 pub mod viz;
 pub mod year_2017;
 pub mod year_2022;
 pub mod year_2023;
+
+pub fn get_input(year: &str, day: &str, input: Option<&str>) -> String {
+    let input = match input {
+        Some(input) => format!("input-{}", input),
+        None => "input".to_string(),
+    };
+    let path = format!("./{}/{}/{}.txt", year, day, input);
+    fs::read_to_string(path).expect("Should have been able to read the file")
+}
--- src/year_2023/day_01.rs
+++ src/year_2023/day_01.rs
@@ -92,3 +92,38 @@ fn part_2(input: &String) -> u32 {
 
     sum
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::get_input;
+
+    use super::*;
+
+    #[test]
+    fn test_part_1_dummy() {
+        let input = get_input("2023", "01", Some("dummy"));
+        let output = part_1(&input);
+        assert_eq!(output, 142);
+    }
+
+    #[test]
+    fn test_part_1() {
+        let input = get_input("2023", "01", None);
+        let output = part_1(&input);
+        assert_eq!(output, 54968);
+    }
+
+    #[test]
+    fn test_part_2_dummy() {
+        let input = get_input("2023", "01", Some("dummy"));
+        let output = part_2(&input);
+        assert_eq!(output, 281);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = get_input("2023", "01", None);
+        let output = part_2(&input);
+        assert_eq!(output, 54094);
+    }
+}
