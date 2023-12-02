457a86c526838fc44270cddc7261af00d70ef5af
Cameron Dubas <camerondubas@gmail.com>
2023-12-02 14:10:52 +0000

    feat: 2023 02 rust

--- benches/bench-2023.rs
+++ benches/bench-2023.rs
@@ -1,18 +1,34 @@
-use aoc::{
-    get_input,
-    year_2023::day_01::{part_1, part_2},
-};
+use aoc::{get_input, year_2023};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn day_01(c: &mut Criterion) {
     let input = get_input("2023", "01", None);
-    c.bench_function("2023 01 Part 1", |b| b.iter(|| part_1(black_box(&input))));
-    c.bench_function("2023 01 Part 2", |b| b.iter(|| part_2(black_box(&input))));
+    c.bench_function("2023 01 Part 1", |b| {
+        b.iter(|| year_2023::day_01::part_1(black_box(&input)))
+    });
+    c.bench_function("2023 01 Part 2", |b| {
+        b.iter(|| year_2023::day_01::part_2(black_box(&input)))
+    });
+}
+
+pub fn day_02(c: &mut Criterion) {
+    let input = get_input("2023", "02", None);
+
+    c.bench_function("2023 02 Part 1", |b| {
+        b.iter(|| year_2023::day_02::part_1(black_box(&input)))
+    });
+    c.bench_function("2023 02 Part 2", |b| {
+        b.iter(|| year_2023::day_02::part_2(black_box(&input)))
+    });
+
+    c.bench_function("2023 02 Part 2 iter", |b| {
+        b.iter(|| year_2023::day_02::part_2_iter(black_box(&input)))
+    });
 }
 
 criterion_group! {
   name =  year_2023;
   config = Criterion::default().sample_size(1000);
-  targets = day_01
+  targets = day_01, day_02
 }
 criterion_main!(year_2023);
---
+++ src/year_2023/day_02.rs
@@ -0,0 +1,137 @@
+use itertools::Itertools;
+
+use crate::runner::run;
+
+pub fn solution(contents: String) {
+    run("Part 1", part_1, &contents);
+    run("Part 2", part_2, &contents);
+}
+
+const MAX_RED: u32 = 12;
+const MAX_GREEN: u32 = 13;
+const MAX_BLUE: u32 = 14;
+
+#[derive(Clone, Debug, Default)]
+struct Round {
+    pub red: u32,
+    pub green: u32,
+    pub blue: u32,
+}
+
+impl Round {
+    fn is_valid(&self) -> bool {
+        self.red <= MAX_RED && self.green <= MAX_GREEN && self.blue <= MAX_BLUE
+    }
+}
+
+impl From<&str> for Round {
+    fn from(val: &str) -> Self {
+        val.split(", ")
+            .map(|color| color.split_once(' ').unwrap())
+            .fold(Round::default(), |mut acc, (value, color)| {
+                let value = value.parse().unwrap();
+                match color {
+                    "red" => acc.red = value,
+                    "green" => acc.green = value,
+                    "blue" => acc.blue = value,
+                    _ => panic!("Invalid color"),
+                };
+                acc
+            })
+    }
+}
+
+#[derive(Debug)]
+struct Game {
+    pub id: u32,
+    rounds: Vec<Round>,
+}
+
+impl Game {
+    fn new(input: &str) -> Self {
+        let (id_section, rounds_section) = input.split_once(": ").unwrap();
+        let (_, id) = id_section.split_once(' ').unwrap();
+
+        let rounds = rounds_section
+            .split("; ")
+            .map(|round| round.into())
+            .collect_vec();
+
+        Self {
+            id: id.parse().unwrap(),
+            rounds,
+        }
+    }
+
+    fn is_valid(&self) -> bool {
+        self.rounds.iter().all(|round| round.is_valid())
+    }
+
+    fn value(&self) -> Option<u32> {
+        if self.is_valid() {
+            Some(self.id)
+        } else {
+            None
+        }
+    }
+
+    fn min_capacity(&self) -> [u32; 3] {
+        self.rounds.iter().fold([0, 0, 0], |mut capacity, round| {
+            capacity[0] = capacity[0].max(round.red);
+            capacity[1] = capacity[1].max(round.green);
+            capacity[2] = capacity[2].max(round.blue);
+            capacity
+        })
+    }
+
+    fn power(&self) -> u32 {
+        self.min_capacity().into_iter().product()
+    }
+}
+
+#[allow(clippy::all)]
+pub fn part_1(input: &String) -> u32 {
+    input
+        .lines()
+        .filter_map(|line| Game::new(line).value())
+        .sum()
+}
+
+pub fn part_2(input: &String) -> u32 {
+    input.lines().map(|line| Game::new(line).power()).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::get_input;
+
+    use super::*;
+
+    #[test]
+    fn test_part_1_dummy() {
+        let input = get_input("2023", "02", Some("dummy"));
+        let output = part_1(&input);
+        assert_eq!(output, 8);
+    }
+
+    #[test]
+    fn test_part_1() {
+        let input = get_input("2023", "02", None);
+        let output = part_1(&input);
+        assert_eq!(output, 2593);
+    }
+
+    #[test]
+    fn test_part_2_dummy() {
+        let input = get_input("2023", "02", Some("dummy"));
+        let output = part_2(&input);
+        assert_eq!(output, 2286);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = get_input("2023", "02", None);
+        let output = part_2(&input);
+        assert_eq!(output, 54699);
+    }
+}
