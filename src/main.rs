6a6fb2fb5eccc408bade54fdbad516ff5051628e
Cameron Dubas <camerondubas@gmail.com>
2023-12-06 14:46:25 +0000

    feat: main fn template

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+/// Useful for running a given day's code, outside of the test suite.
+fn main() {
+    // let input = aoc::get_input("2023", "05", None);
+    // let output = aoc::year_2023::day_05::part_2(&input);
+    // println!("{}", output);
+}
