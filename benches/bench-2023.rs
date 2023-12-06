e0a701ca36d3d7bb4a8dfea085d736acf31fb892
Cameron Dubas <camerondubas@gmail.com>
2023-12-06 14:44:56 +0000

    perf: add benches for 2023 05 06

--- benches/bench-2023.rs
+++ benches/bench-2023.rs
@@ -44,5 +44,29 @@ pub fn day_04(c: &mut Criterion) {
     });
 }
 
-criterion_group!(year_2023, day_01, day_02, day_03, day_04);
+pub fn day_05(c: &mut Criterion) {
+    let input = get_input("2023", "05", None);
+
+    c.bench_function("2023 05 Part 1", |b| {
+        b.iter(|| year_2023::day_05::part_1(black_box(&input)))
+    });
+
+    //  //Too slow
+    // c.bench_function("2023 05 Part 2", |b| {
+    //     b.iter(|| year_2023::day_05::part_2(black_box(&input)))
+    // });
+}
+
+pub fn day_06(c: &mut Criterion) {
+    let input = get_input("2023", "06", None);
+
+    c.bench_function("2023 06 Part 1", |b| {
+        b.iter(|| year_2023::day_06::part_1(black_box(&input)))
+    });
+    c.bench_function("2023 06 Part 2", |b| {
+        b.iter(|| year_2023::day_06::part_2(black_box(&input)))
+    });
+}
+
+criterion_group!(year_2023, day_01, day_02, day_03, day_04, day_05, day_06);
 criterion_main!(year_2023);
